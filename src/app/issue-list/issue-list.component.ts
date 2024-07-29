393e01316b8fde28500b93b2c26d40c2a0f9a5e7
Itallo Ramon <irvp@ic.ufal.br>
2024-07-29 19:57:52 -0300

    feat: add option to resolve an issue

--- src/app/issue-list/issue-list.component.ts
+++ src/app/issue-list/issue-list.component.ts
@@ -12,6 +12,8 @@ export class IssueListComponent implements OnInit {
 
   showReportIssue = false;
 
+  selectedIssue: Issue | null = null;
+
   constructor(private issueService: IssuesService) {}
 
   ngOnInit(): void {
@@ -25,5 +27,13 @@ export class IssueListComponent implements OnInit {
   onCloseReport() {
     this.showReportIssue = false;
     this.getIssues();
+  }
+
+  onConfirm(confirmed: boolean) {
+    if (confirmed && this.selectedIssue) {
+      this.issueService.completeIssue(this.selectedIssue);
+      this.getIssues();
+    }
+    this.selectedIssue = null;
   }
 }
