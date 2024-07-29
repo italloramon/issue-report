a54d89927875478087bc8c22c4b508c50619933c
Itallo Ramon <irvp@ic.ufal.br>
2024-07-29 19:48:42 -0300

    feat: add validators to form

--- src/app/issue-report/issue-report.component.ts
+++ src/app/issue-report/issue-report.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { IssuesService } from '../issues.service';
 import { Issue } from '../issue';
 
@@ -19,15 +19,20 @@ export class IssueReportComponent {
   @Output() formClose = new EventEmitter();
 
   issueForm = new FormGroup<IssueForm>({
-    title: new FormControl('', { nonNullable: true }),
+    title: new FormControl('', { nonNullable: true, validators: Validators.required }),
     description: new FormControl('', { nonNullable: true }),
-    priority: new FormControl('', { nonNullable: true }),
-    type: new FormControl('', { nonNullable: true })
+    priority: new FormControl('', { nonNullable: true, validators: Validators.required }),
+    type: new FormControl('', { nonNullable: true, validators: Validators.required })
   });
 
   constructor(private issueService: IssuesService) {}
 
   addIssue() {
+    if (this.issueForm && this.issueForm.invalid) {
+      this.issueForm.markAllAsTouched();
+      return;
+    }
+
     this.issueService.createIssue(this.issueForm.getRawValue() as Issue);
     this.formClose.emit();
   }
